8428a4e661e6457804752954414db429fe02bb1f
Dmitry <dimabelou5off@yandex.ru>
2021-10-03 10:07:41 +0300

    implemented 2 tasks

---
+++ conditionals/script.js
@@ -0,0 +1,44 @@
+
+
+//1. пользователь вводит 2 числа, вывести в консоль большее из них
+
+// let a = +prompt('one');
+// let b = +prompt('two');
+
+
+// if (a > b) {
+//     console.log(a);
+// } else if (b > a ) {
+//     console.log(b);
+// }
+
+
+
+
+
+//2. пользователь вводит 3 числа, первые два это числа которые он хочет перемножить
+// третье число, это результат умножения по мнению пользователя
+// выяснить правильно ли ответил пользователь и вывести в консоль ОК если правильно
+// NOT OK {rightAnswer} если неправильно
+
+
+
+// let num1 = +prompt('enter num1');
+// let num2 = +prompt('enter num2');
+// let userResult = +prompt('enter result');
+
+// const rightanswer = num1 * num2;
+
+// if (userResult === rightanswer) {
+//     console.log(`OK`);
+// } else  {
+//     console.log(`NOT OK ${rightanswer}`)
+// }
+
+
+
+
+
+
+
+
