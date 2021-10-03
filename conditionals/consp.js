44329002dd26f34e9910b7061999ada585c9b827
Dmitry <dimabelou5off@yandex.ru>
2021-10-03 11:19:51 +0300

    switch, ternary

---
+++ conditionals/consp.js
@@ -0,0 +1,68 @@
+
+// let userDay = prompt('enter day of weak');
+
+// if ( isNaN(userDay)  ) {
+//         console.log('incrorrect data');
+//     } else if ( userDay === null ) {
+//         console.log('you cancelled');
+//     } else {
+//         userDay = +userDay;
+//         userDay = +userDay;
+    
+
+//         switch(userDay){
+//             case 1:
+//                 console.log('Monday');
+//                 break;    
+//             case 2:   
+//                 console.log('Tuesday') 
+//                 break;
+//             case 3:
+//                 console.log('Wednesday');
+//                 break;
+//             case 4:
+//                 console.log('Thursday');
+//                 break;
+//             case 5:
+//                 console.log('Fiday');
+//                 break;
+//             case 6:
+//                 console.log('Saturday');
+//                 break;
+//             case 7:
+//                 console.log('Sunday');
+//                 break;
+//             default :
+//                 console.log('Error');
+//                 break;
+//         }
+
+//     }
+
+
+// 2. Переписать в тернарный оператор
+// x ? y : z
+// let age = 20;
+
+// if (age < 10) {
+//     console.log('HI!');
+// } else if (age < 20) {
+//     console.log('Hello!');
+// } else {
+//     console.log('Good day!');
+// }
+
+// console.log(
+//     age < 10 
+//     ? 'Hi!'
+//     : age < 20 
+//         ? 'Hello!'
+//         : 'Good day!'
+// );
+
+
+
+
+
+
+
